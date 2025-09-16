1f19894c589fc13d33b6448a02ba0192b0ba20a5
6et3ct1ve <6et3ct1ve@gmail.com>
2025-09-16 09:03:12 +0300

    Add urls for pages

--- places/urls.py
+++ places/urls.py
@@ -5,4 +5,7 @@ app_name = 'places'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('places/', views.places_list, name='places_list'),
+    path('places/add/', views.add_place, name='add_place'),
+    path('places/<int:place_id>/', views.place_detail, name='place_detail'),
 ]
